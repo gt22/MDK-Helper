35036950a43c14b4eda5bcbb5b02163c78eb7cd8
gt22 <igorengel@mail.ru>
2016-07-21 23:19:45 +0300

    -1 возможная ошибка при запуске не с винды (Он всё ещё не заработает, но мы правим)

--- java/com/gui/Gui.java
+++ java/com/gui/Gui.java
@@ -3,16 +3,20 @@ package com.gui;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import com.creatix.autodecompiler.AutoDecompiler;
 import com.gt22.generator.Constants;
-
 
 public class Gui
 {
 	public static void init()
 	{
-		try {
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); 
-		} catch (Exception e) {
+		try
+		{
+			if (AutoDecompiler.isWindows())
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 		SwingUtilities.invokeLater(new Runnable()
@@ -22,6 +26,6 @@ public class Gui
 			{
 				new MainFrame("MDK helper");
 			}
-		});	
+		});
 	}
 }
