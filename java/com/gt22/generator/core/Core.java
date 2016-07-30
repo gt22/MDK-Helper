1d1882ea3065c0287b82f3182ce9475cde1e7c86
gt22 <igorengel@mail.ru>
2016-07-30 06:42:57 +0300

    Там чутка дебага осталось

--- java/com/gt22/generator/core/Core.java
+++ java/com/gt22/generator/core/Core.java
@@ -8,14 +8,6 @@ public class Core
 {
 	public static void main(String[] args) throws IOException
 	{
-		if(args[0].equals("--default") || args[0].equals("-d"))
-		{
-			Writer.generateMod("q", "q", "q", "1.7.10", new File("F:modding\\eclipse\\test"), false, true);
-		}
-		else
-		{
-			System.out.println(args[0]);
-		}
 		Gui.init();
 	}
 }
